842b0802ce91e8465c5ef9be173c84d9d52f888e
jimmyndegizi <ndegwajames123@gmail.com>
2024-10-03 09:00:15 +0300

    did all the questions but not showing passed tests

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// multiply
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+// random
+const random = math.floor(math.random() * 100) + 1;
+
+
+// mod
+let num3 = 14; 
+let num4 = 5;  
+let mod = num3 % num4;
+
+
+// max
+const max= math.max(15,16,17,18,19,20);
+
+
+
+
